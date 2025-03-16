f40eb659bbe467d351eae8d7de34c7c5deb8f227
Ch1ckenMax <li-hk@hotmail.com>
2025-03-17 00:28:01 +0800

    test bench update

--- TestBench/arduino_test/arduino_test.ino
+++ TestBench/arduino_test/arduino_test.ino
@@ -17,58 +17,27 @@ DFRobot_MCP2515 CAN(SPI_CS_PIN);   // Set CS pin
 
 class PerformanceMonitor{
 public:
-PerformanceMonitor(const uint32_t print_interval, unsigned int timestamp_buffer_size):
+PerformanceMonitor(const uint32_t print_interval):
     INTERVAL{print_interval},
     m_last_check{0},
-    m_receive_count{0},
-    m_timestamp_buffer_size{timestamp_buffer_size},
-    m_timestamp_buffer_next_index{0}
-{
-    m_receive_timestamp_buffer = new unsigned long[m_timestamp_buffer_size];
-}
+    m_receive_count{0}
+{}
 
-~PerformanceMonitor()
+void CheckMessageReceived()
 {
-    delete [] m_receive_timestamp_buffer;
-}
-
-void AddCount(unsigned long count)
-{
-  m_receive_count += count;
-}
-
-void RecordReceiveTime(const unsigned long current_time)
-{
-    // Overflown.
-    if (m_timestamp_buffer_next_index == m_timestamp_buffer_size)
-    {
-        return;
-    }
-    m_receive_timestamp_buffer[m_timestamp_buffer_next_index++] = current_time;
+  if (CAN_MSGAVAIL == CAN.checkReceive())   // check if data coming
+  {
+      CAN.readMsgBuf(&m_len, m_buf);   // remove data from the buffer.
+      const auto id = CAN.getCanId();
+      if (id == 0x0C0) m_receive_count += 1;
+  }
 }
 
 void Print(const unsigned long current_time)
 {
-    if (current_time < m_last_check + INTERVAL)
-    {
-        return;
-    }
-
-    // Time to log
-    PrintFrequency();
-
-    if (m_timestamp_buffer_next_index < 2)
-    {
-        Serial.println("Less than two messages received. Not printing average latency.");
-        // Clean up
-        ResetMonitor(current_time);
-        return;
-    }
-    const auto average_latency = GetAverageLatency();
-    PrintAverageLatency(average_latency);
-
-    // Clean up
-    ResetMonitor(current_time);
+    if (current_time < m_last_check + INTERVAL) return;
+    PrintFrequency(); // Time to log
+    ResetMonitor(current_time); // Clean up
 }
 
 private:
@@ -76,64 +45,18 @@ private:
 void PrintFrequency() const
 {
     Serial.print(m_receive_count / ((float)INTERVAL/ 1000));
-    Serial.print(' ');
-}
-
-void PrintAverageLatency(const unsigned long average_latency) const
-{
-    Serial.print(average_latency);
-    Serial.println();
-}
-
-unsigned long GetAverageLatency() const
-{
-    unsigned long total = 0;
-    for (unsigned int i = 0; i < m_timestamp_buffer_next_index - 1; i++)
-    {
-        total += m_receive_timestamp_buffer[i + 1] - m_receive_timestamp_buffer[i];
-    }
-    return total / (m_timestamp_buffer_next_index - 1);
+    Serial.println(' ');
 }
 
 void ResetMonitor(const unsigned long current_time)
 {
     m_last_check = current_time;
     m_receive_count = 0;
-    m_timestamp_buffer_next_index = 0;
 }
 
 const uint32_t INTERVAL;
 unsigned long m_last_check;
 unsigned long m_receive_count;
-unsigned long* m_receive_timestamp_buffer;
-unsigned int m_timestamp_buffer_size;
-unsigned int m_timestamp_buffer_next_index;
-};
-
-class CANMessageReceiveBlackHole
-{
-public:
-CANMessageReceiveBlackHole():
-    m_len{0}
-{}
-
-// Return true if a command message from the VCU is received.
-bool VCUCommandMessageReceived()
-{
-    if (CAN_MSGAVAIL == CAN.checkReceive())   // check if data coming
-    {
-        CAN.readMsgBuf(&m_len, m_buf);   // remove data from the buffer.
-        const auto id = CAN.getCanId();
-        if (id == 0x0C0)
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-private:
 unsigned char m_len = 0;
 unsigned char m_buf[8];
 };
@@ -141,18 +64,27 @@ unsigned char m_buf[8];
 class CANMessageFloodTransmitHelper
 {
 public:
-CANMessageFloodTransmitHelper(const uint32_t pong_can_id, const uint32_t interval_ms, const unsigned int messages_per_interval):
+CANMessageFloodTransmitHelper(const uint32_t pong_can_id, const unsigned int messages_per_interval):
     PONG_CAN_ID{pong_can_id},
-    INTERVAL{interval_ms},
-    MSG_PER_INTERVAL{messages_per_interval},
-    m_last_check{0}
+    msg_per_interval{messages_per_interval},
+    m_last_check{0},
+    m_last_interval_increase{0}
 {}
 
 Transmit(const unsigned long current_time)
 {
-    if (current_time >= m_last_check + INTERVAL)
+    if (current_time / 3000 != m_last_interval_increase)
     {
-        for (unsigned int i = 0; i < MSG_PER_INTERVAL; i++)
+       msg_per_interval += 1;
+       Serial.print("Interval is now increased to ");
+       Serial.print(msg_per_interval);
+       Serial.println();
+       m_last_interval_increase = current_time / 3000;
+    }
+
+    if (current_time > m_last_check)
+    {
+        for (unsigned int i = 0; i < msg_per_interval; i++)
         {
           CAN.sendMsgBuf(PONG_CAN_ID, 0, 8, buf);
         }
@@ -162,28 +94,25 @@ Transmit(const unsigned long current_time)
 
 private:
 const uint32_t PONG_CAN_ID;
-const uint32_t INTERVAL;
-const unsigned int MSG_PER_INTERVAL;
+unsigned int msg_per_interval;
 uint32_t m_last_check;
+uint32_t m_last_interval_increase;
 unsigned char buf[8]; // Garbage value is okay. we won't need to transmit meaningful data.
 };
 
-const uint32_t PONG_CAN_ID = 0x0D0;
-const uint32_t flood_interval_ms = 1;
-const unsigned int flood_messages_per_interval = 0;
+const uint32_t PONG_CAN_ID = 0x0A5;
+const unsigned int flood_messages_per_ms = 0;
 
-CANMessageFloodTransmitHelper canMsgTxHelper(PONG_CAN_ID, flood_interval_ms, flood_messages_per_interval);
+CANMessageFloodTransmitHelper canMsgTxHelper(PONG_CAN_ID, flood_messages_per_ms);
 
 const uint32_t performance_print_interval_ms = 500;
-const unsigned int performance_monitor_buffer_size = 200;
-
-PerformanceMonitor performanceMonitor(performance_print_interval_ms, performance_monitor_buffer_size);
-CANMessageReceiveBlackHole canMsgRxBlackhole;
+PerformanceMonitor performanceMonitor(performance_print_interval_ms);
 
 void setup()
 {
     Serial.println("Observor initializing...");
     Serial.begin(115200);
+
 
     while( CAN.begin(CAN_500KBPS) ){   // init can bus : baudrate = 500k
         Serial.println("DFROBOT's CAN BUS Shield init fail");
@@ -191,16 +120,16 @@ void setup()
         delay(3000);
     }
     Serial.println("DFROBOT's CAN BUS Shield init ok!\n");
+    uint8_t buf[8];
+    const uint32_t id = 0x0A5;
+    CAN.sendMsgBuf(id, 0, 8, buf);
 
 }
 
 void loop()
 {
     const auto current_time = millis();
-    if (canMsgRxBlackhole.VCUCommandMessageReceived())
-    {
-        performanceMonitor.AddCount(1);
-        performanceMonitor.RecordReceiveTime(current_time);   }
+    performanceMonitor.CheckMessageReceived();
     
     performanceMonitor.Print(current_time);
     canMsgTxHelper.Transmit(current_time);
